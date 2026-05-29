0d13e53da3f91a2f7755444c1468760bd5ec4ddc
andriibabiuk <andrii.babiuk.dev@gmail.com>
2026-05-29 17:47:47 +0300

    feat: add movie list display on main page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
-
+const request = require('request');
+const apiKey = process.env.API_KEY;
+const apiBaseUrl = 'http://api.themoviedb.org/3';
+const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
+const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
+router.use((req, res, next) => {
+	res.locals.imageBaseUrl = imageBaseUrl;
+	next();
+});
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+	request(nowPlayingUrl, (error, response, movieData) => {
+		const parsedData = JSON.parse(movieData);
+		res.render('index', { parsedData: parsedData.results });
+	});
 });
 
 module.exports = router;
